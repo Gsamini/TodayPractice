2abc7382a2889ee81e2d9d85b58bc194fcfa9a98
Gsamini <105210011+Gsamini@users.noreply.github.com>
2022-05-13 09:06:24 -1200

    We add value to the string

--- src/newprojectpractice/GroupStudy.java
+++ src/newprojectpractice/GroupStudy.java
@@ -3,8 +3,9 @@ package newprojectpractice;
 public class GroupStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String d ="ahmad";
+		String b ="Rohulla";
 	}
+
 
 }
